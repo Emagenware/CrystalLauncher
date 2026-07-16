243091deb2853f27c58afc883902a214b1586aa2
jinbao4 <aitabdallahabdarrahman@gmail.com>
2026-07-16 17:21:31 +0100

    scaffold react frontend for migration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,32 @@
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import path from "path";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [tailwindcss(), svelte()],
+// @ts-expect-error process is a nodejs global
+const host = process.env.TAURI_DEV_HOST;
+
+// https://vite.dev/config/
+export default defineConfig(async () => ({
+  plugins: [react()],
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent Vite from obscuring rust errors
   clearScreen: false,
-  resolve: { alias: { $lib: path.resolve("./src/lib") } },
-  server: { port: 8080, strictPort: true }
-});
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell Vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+}));
